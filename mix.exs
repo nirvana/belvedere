544aeb1c2513436f041ece310c99ebb55099bb97
chenso <chen.s@wustl.edu>
2015-06-11 15:23:04 -0400

    removed reps from template project

--- mix.exs
+++ mix.exs
@@ -33,8 +33,6 @@ defmodule Belvedere.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-    {:earmark, "~> 0.1", only: :dev},
-    {:ex_doc, "~> 0.7", only: :dev}
     ]
   end
 
