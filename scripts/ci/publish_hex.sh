eb5059e5777f9689edc53ec579183215a0164a93
chenso <chen.s@wustl.edu>
2015-06-11 14:49:47 -0400

    -

--- scripts/ci/publish_hex.sh
+++ scripts/ci/publish_hex.sh
@@ -6,5 +6,8 @@ git clone https://github.com/elixir-lang/ex_doc.git
 cd ex_doc
 mix do deps.get, compile
 
+cd /$HOME/$CIRCLE_PROJECT_REPONAME
+mix compile
+
 /$HOME/dependencies/ex_doc/bin/ex_doc $CIRCLE_PROJECT_REPONAME $PROJECT_VERSION path/to/project/ebin -m $PROJECT_MODULE -u https://github.com/$GITHUB_USER/$GITHUB_REPO
 
