a984997c2feacedffada4f621ee98bcf378d3cba
chenso <chen.s@wustl.edu>
2015-06-12 13:42:09 -0400

    Update publish_hex.sh

--- scripts/ci/publish_hex.sh
+++ scripts/ci/publish_hex.sh
@@ -3,7 +3,7 @@ git clone https://github.com/elixir-lang/ex_doc.git
 cd ex_doc
 mix do deps.get, deps.compile, compile
 
-cd /$HOME/$CIRCLE_PROJECT_REPONAME
+cd /$HOME/dependencies/ex_doc/bin
 mix compile
 
 /$HOME/dependencies/ex_doc/bin/ex_doc $PROJECT_NAME $PROJECT_VERSION $HOME/$CIRCLE_PROJECT_REPONAME/ebin -m $PROJECT_MODULE$ -u https://github.com/$GITHUB_USER/$GITHUB_REPO
