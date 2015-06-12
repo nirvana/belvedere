6321d68bf5ee2d341cdd978b7853f5dd58d93d0b
chenso <chen.s@wustl.edu>
2015-06-12 14:15:34 -0400

    Update publish_hex.sh

--- scripts/ci/publish_hex.sh
+++ scripts/ci/publish_hex.sh
@@ -6,7 +6,7 @@ mix do deps.get, deps.compile, compile
 cd /$HOME/dependencies/ex_doc/bin
 mix compile
 
-/$HOME/dependencies/ex_doc/bin/ex_doc $PROJECT_NAME $PROJECT_VERSION $HOME/$CIRCLE_PROJECT_REPONAME/ebin -m $PROJECT_MODULE$ -u https://github.com/$GITHUB_USER/$GITHUB_REPO
+/$HOME/dependencies/ex_doc/bin/ex_doc $PROJECT_NAME $PROJECT_VERSION /$HOME/$CIRCLE_PROJECT_REPONAME/ebin -m $PROJECT_MODULE$ -u https://github.com/$GITHUB_USER/$GITHUB_REPO
 
 #cd /$HOME/$CIRCLE_PROJECT_REPONAME/deps/ex_doc
 #mix do deps.get, deps.compile, compile
